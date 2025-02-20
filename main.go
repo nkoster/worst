34bfe19f76e5e734276f33d6ec7788a7cc95ea7c
Niels Koster <n.koster@portavita.eu>
2025-02-20 01:36:33 +0100

    Make it work for the /audiouploader route in the drone existence website

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 var (
@@ -18,9 +19,9 @@ var (
 )
 
 func main() {
-	http.HandleFunc("/upload", uploadHandler)
-	http.HandleFunc("/events", eventsHandler)
-	http.Handle("/", http.FileServer(http.Dir("./ui")))
+	http.HandleFunc("/droneuploader/upload", uploadHandler)
+	http.HandleFunc("/droneuploader/events", eventsHandler)
+	//http.Handle("/droneuploader/ui", http.FileServer(http.Dir("/home/pot/html/ui")))
 
 	fmt.Println("Server running: http://localhost:3456")
 	if err := http.ListenAndServe(":3456", nil); err != nil {
@@ -39,12 +40,14 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	const maxUploadSize = 1024 * 1024 * 1024 // 1GB
 	if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+		fmt.Println("ERROR: File too big.")
 		http.Error(w, "File too big", http.StatusBadRequest)
 		return
 	}
 
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
+		fmt.Println("ERROR: Invalid file: ", err)
 		http.Error(w, "Invalid file", http.StatusBadRequest)
 		return
 	}
@@ -57,29 +60,34 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// .mp3,.wav,.ogg,.aiff,.aac,.m4a,.opus,.flac,.wma
 	allowedExts := map[string]bool{".mp3": true, ".wav": true, ".ogg": true, ".aiff": true, ".aac": true, ".m4a": true, ".opus": true, ".flac": true, ".wma": true}
 	if _, ok := allowedExts[fileExt]; !ok {
+		fmt.Println("ERROR: File format not allowed.")
 		http.Error(w, "File format not allowed", http.StatusBadRequest)
 		return
 	}
 
 	dst, err := os.Create(filePath)
 	if err != nil {
+		fmt.Println("ERROR: Cannot create file: ", err)
 		http.Error(w, "Cannot create file", http.StatusInternalServerError)
 		return
 	}
 	defer dst.Close()
 
 	if _, err := io.Copy(dst, file); err != nil {
+		fmt.Println("ERROR: Cannot save file: ", err)
 		http.Error(w, "Cannot save file", http.StatusInternalServerError)
 		return
 	}
 
+	fmt.Println("File uploaded: ", filePath)
 	// ffmpegCmd := fmt.Sprintf("timeout --foreground 25 ffmpeg -i \"%s\" -af loudnorm=I=-16:dual_mono=true:TP=-1.5:LRA=11:print_format=summary -f null -", filePath)
-	ffmpegCmd := fmt.Sprintf("ffmpeg -i \"%s\" -y -filter_complex \"aformat=channel_layouts=stereo,showwavespic=s=700x120:colors=0D6EFD|0000000\" -frames:v 1 \"./ui/%s.png\" -af loudnorm=I=-16:dual_mono=true:TP=-1.5:LRA=11:print_format=summary -f null -", filePath, filePath)
+	ffmpegCmd := fmt.Sprintf("ffmpeg -i \"%s\" -y -filter_complex \"aformat=channel_layouts=stereo,showwavespic=s=700x120:colors=0D6EFD|0000000\" -frames:v 1 \"/home/pot/html/audiouploader/%s.png\" -af loudnorm=I=-16:dual_mono=true:TP=-1.5:LRA=11:print_format=summary -f null -", filePath, filePath)
 
 	go func() {
 		output, err := executeFFmpegCommand(ffmpegCmd)
 		if err != nil {
 			// ffmpeg error(s)
+			fmt.Println("ERROR: ", err)
 			log.Printf("ffmpeg error: %v\n%v", err, ffmpegCmd)
 			commandOutputChan <- "ffmpeg error"
 			return
@@ -94,6 +102,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 func eventsHandler(w http.ResponseWriter, r *http.Request) {
 	flusher, ok := w.(http.Flusher)
 	if !ok {
+		fmt.Println("ERROR: Streaming not supported")
 		http.Error(w, "Streaming not supported", http.StatusInternalServerError)
 		return
 	}
@@ -102,10 +111,16 @@ func eventsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
 	for {
 		select {
 		case output := <-commandOutputChan:
 			fmt.Fprintf(w, "data: %s\n\n", strings.ReplaceAll(output, "\n", "\\n"))
+			flusher.Flush()
+		case <-ticker.C:
+			fmt.Fprintf(w, "data: ping\n\n")
 			flusher.Flush()
 		case <-r.Context().Done():
 			return
@@ -123,6 +138,7 @@ func executeFFmpegCommand(cmd string) (string, error) {
 	output := outputBuffer.String()
 
 	if err != nil {
+		fmt.Println("ERROR: ", err)
 		if exitErr, ok := err.(*exec.ExitError); ok && exitErr.ExitCode() == 124 {
 			// Behandel exit status 124 (timeout) als een succesvolle afronding
 			return processFFmpegOutput(output), nil
